343b88fb89c70597fa1882e3c4f4d7e17ad75cd8
Frizlab <fload@me.com>
2017-02-12 19:33:51 +0100

    Initial Commit

---
+++ base64/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  base64
+//
+//  Created by François Lamboley on 2/12/17.
+//  Copyright © 2017 happn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ base64/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  base64
+//
+//  Created by François Lamboley on 2/12/17.
+//  Copyright © 2017 happn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
