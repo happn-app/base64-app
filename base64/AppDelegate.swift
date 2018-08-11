6d4daac019cdf9b7403d81feafb1bdf8fd90df8a
Frizlab <fload@me.com>
2018-08-11 19:45:59 +0200

    Upgrade project for latest Xcode (10 beta 5)

--- base64/AppDelegate.swift
+++ base64/AppDelegate.swift
@@ -14,11 +14,9 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 	
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
 	}
 	
 	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
 	}
 	
 	func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
